b94c2d5d45b2172b6c27af670a13faa842e85ff8
Erico Silva <augusto.ericosilva@gmail.com>
2021-10-02 21:21:47 -0400

    Add routes for csv and json formats; add CSV and JSON downloads

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Render, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Render, Req, Res } from '@nestjs/common';
 import { PlayersService } from './players.service';
 import { CreatePlayerDto } from './dto/create-player.dto';
 import { UpdatePlayerDto } from './dto/update-player.dto';
-import { Request } from 'express';
+import { Request, response } from 'express';
 import { parse } from './parsers/filters';
 import { parse as parseToRespose } from './parsers/responses';
+import { parse as parseToCsv } from 'json2csv';
 
 @Controller('players')
 export class PlayersController {
@@ -18,6 +19,22 @@ export class PlayersController {
   @Get()
   @Render('players/index')
   findAll(@Req() request: Request) {
+    return this.doReadMany(request);
+  }
+
+  @Get('?.json')
+  readManyJson(@Req() request: Request) {
+    return this.doReadMany(request);
+  }
+
+  @Get('?.csv')
+  readManyCsv(@Req() request: Request) {
+    const opts = { fields: [ 'player', 'yds', 'td', 'lng' ] };
+    const result = this.doReadMany(request);
+    return parseToCsv(result.items, opts);
+  }
+
+  doReadMany(request: Request) {
     const filter = parse(request.query as any);
     const result = this.playersService.findAll(filter);
     return parseToRespose(result);
